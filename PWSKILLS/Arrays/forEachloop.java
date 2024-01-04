1cacb2da7a726cd79f11cf35dc2193f916f94e23
SAKSHAMGARG2124 <sakshamgarggarg83@gmail.com>
2024-01-05 00:49:06 +0530

    Arr in java

---
+++ PWSKILLS/Arrays/forEachloop.java
@@ -0,0 +1,27 @@
+
+
+// import java.util.Scanner;
+
+// public class forEachloop {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         // int n = sc.nextInt();
+
+//         // 1D array
+//         // int nums[]= {1,2,3,4};
+//         // for (int i : nums) {
+//         //     System.out.println(i);
+//         // }
+
+//         // 2D Array
+//         int nums[][]={{1,2},{3,4},{5,6}};
+//         for (int i[] : nums) {
+//             for (int j : i) {
+//                 System.out.print(j+" ");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+
+// }
