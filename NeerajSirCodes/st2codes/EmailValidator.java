f325ef8de7f644ff3555a09963787926b33e497d
SAKSHAMGARG21 <sakshamgarggarg83@gmail.com>
2024-09-17 15:40:08 +0530

    done

--- NeerajSirCodes/st2codes/EmailValidator.java
+++ NeerajSirCodes/st2codes/EmailValidator.java
@@ -7,43 +7,27 @@ class MyException extends Exception {
 }
 
 public class EmailValidator {
-    public static void emailValidator(String email) {
+    public static void emailValidator(String email) throws MyException {
         // Write your code here
-        int es = email.length();
+        int es = email.length() - 1;
         int dotidx = email.indexOf(".");
         int atidx = email.indexOf("@");
         int lastdotidx = email.lastIndexOf(".");
         int lastatidx = email.lastIndexOf("@");
 
-        if (dotidx == -1 || atidx == -1 || lastatidx==es-1 || lastdotidx==es-1) {
-            System.out.println("Invalid vaild");
-            return;
-        }
-
-        for (int i = 0; i < email.length(); i++) {
-            if (dotidx > 0 && atidx > 0 && dotidx < es && atidx < es) {
-                if (dotidx < atidx - 1 || atidx + 1 < dotidx) {
-                    System.out.println("Valid Email");
-                    return;
-                } else {
-                    System.out.println("Invalid email");
-                    return;
-                }
-            } else {
-                System.out.println("Invalid email");
-                return;
-            }
+        if (dotidx == -1 || atidx == -1 || lastatidx == es || lastdotidx == es || dotidx == 0 || atidx ==  0 || dotidx == atidx - 1 || atidx + 1 == dotidx) {
+            throw new MyException("Invalid Email");
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String email = sc.nextLine();
-        emailValidator(email);
-        // System.out.println("Valid Email");
-        // try {
-        // } catch (MyException e) {
-        //     System.out.println("Error " + e.getMessage());
-        // }
+        try {
+            emailValidator(email);
+            System.out.println("Valid Email");
+        } catch (MyException e) {
+            System.out.println("Error " + e.getMessage());
+        }
     }
 }
